ef4c520902572c8063ce855fd6872a2b3715de00
Théo Joubert <theojoubertmoureaud@gmail.com>
2018-08-15 15:32:47 +0000

    Develop -> Master

--- src/app/shared/global/constants.ts
+++ src/app/shared/global/constants.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
   public static API_ENDPOINT = 'http://35.180.98.237:8080';
-  public static APP_URL = 'http://localhost:4200';
+  public static APP_URL = 'http://ec2-35-180-98-237.eu-west-3.compute.amazonaws.com';
 }
