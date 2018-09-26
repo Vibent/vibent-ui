1a7bdaadd8dcd553544338969fde4c96c7cb1f0e
Conor Ryan <conor.fr.ryan@gmail.com>
2018-09-26 10:29:44 +0000

    Update for new deploy

--- src/app/shared/global/constants.ts
+++ src/app/shared/global/constants.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
-  public static API_ENDPOINT = 'http://35.180.98.237:8080';
-  public static APP_URL = 'http://ec2-35-180-98-237.eu-west-3.compute.amazonaws.com';
+  public static API_ENDPOINT = 'http://api.dev.vibent.com';
+  public static APP_URL = 'http://dev.vibent..com';
 }
 
 export class LoaderSize {
