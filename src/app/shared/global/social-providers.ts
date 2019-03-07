f74b5eef73ec7b075ee7cd247aaf8eed4595e041
Conor Ryan <conor.fr.ryan@gmail.com>
2019-03-07 11:37:40 +0100

    Added different envs for social providers

--- src/app/shared/global/social-providers.ts
+++ src/app/shared/global/social-providers.ts
@@ -1,11 +1,27 @@
 import { Provider } from '../models/provider';
 import { FacebookLoginProvider, GoogleLoginProvider } from 'angularx-social-login';
+import { environment } from '../../../environments/environment';
 
-const SOCIAL_PROVIDERS: Provider[] = [
+const DEV_SOCIAL_PROVIDERS: Provider[] = [
   {
     id: GoogleLoginProvider.PROVIDER_ID,
     label: 'Google',
     provider: new GoogleLoginProvider('821632933125-0re06sp9j4l9nurpql79glsfm4k64sai.apps.googleusercontent.com'),
+    color: '#DC483C',
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    label: 'Facebook',
+    provider: new FacebookLoginProvider('550656608676022'),
+    color: '#3B5998',
+  }
+];
+
+const PROD_SOCIAL_PROVIDERS: Provider[] = [
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    label: 'Google',
+    provider: new GoogleLoginProvider('634029875861-09kdkppk1khbs7krahm18tcqmggts3ee.apps.googleusercontent.com'),
     color: '#DC483C',
   },
   {
@@ -16,4 +32,10 @@ const SOCIAL_PROVIDERS: Provider[] = [
   }
 ];
 
+let SOCIAL_PROVIDERS;
+if(environment.prod){
+  SOCIAL_PROVIDERS = PROD_SOCIAL_PROVIDERS;
+} else {
+  SOCIAL_PROVIDERS = DEV_SOCIAL_PROVIDERS;
+}
 export default SOCIAL_PROVIDERS;
