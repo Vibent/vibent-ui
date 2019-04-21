9f6ab6ad346a2def2602f584be8a7a6be5fab249
Conor Ryan <conor.fr.ryan@gmail.com>
2019-04-22 01:17:50 +0200

    Checkbox bubble redesign

--- src/app/core/services/bubbles-services/checkbox/data/checkbox-data.service.ts
+++ src/app/core/services/bubbles-services/checkbox/data/checkbox-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { UserManagementService } from '../../../user-management.service';
 import { User } from '../../../../../shared/models/user';
 import { HttpService } from '../../../../http/http.service';
-import { CheckboxDataModel, CheckboxOption } from '../../../../../shared/models/bubbles/CheckboxBubble';
+import { CheckboxBubble, CheckboxDataModel, CheckboxOption } from '../../../../../shared/models/bubbles/CheckboxBubble';
 
 @Injectable()
 export class CheckboxDataService {
@@ -35,4 +35,30 @@ export class CheckboxDataService {
     return checkboxOption.userRef === this.user.ref;
   }
 
+  getUserActivity(bubble: CheckboxBubble): string[] {
+    const options = bubble.options;
+    let userRefs = [];
+    if (options.length !== 0) {
+      userRefs = userRefs.concat(options.map(o => o.userRef));
+
+      const optionAnswers = options.map(o => o.answers);
+      if (optionAnswers.length !== 0) {
+        const answers = optionAnswers.reduce((c, n) => c.concat(n));
+        userRefs = userRefs.concat(answers.map(a => a.userRef));
+      }
+    }
+    return userRefs;
+  }
+
+  getAmountOptions(bubble: CheckboxBubble): number {
+    return bubble.options.length;
+  }
+
+  getAmountUnchecked(bubble: CheckboxBubble): number {
+    return bubble.options.filter(o => o.answers.length === 0).length;
+  }
+
+  getUncheckedOptions(bubble: CheckboxBubble, maxAmount: number): CheckboxOption[] {
+    return bubble.options.filter(o => o.answers.length === 0).slice(0, maxAmount);
+  }
 }
--- src/app/modules/pages/event/bubbles-preview/checkbox-bubble/checkbox-bubble-preview.component.ts
+++ src/app/modules/pages/event/bubbles-preview/checkbox-bubble/checkbox-bubble-preview.component.ts
@@ -1,19 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CheckboxBubble } from '../../../../../shared/models/bubbles/CheckboxBubble';
+import { CheckboxDataService } from '../../../../../core/services/bubbles-services/checkbox/data/checkbox-data.service';
 
 @Component({
-  selector: 'app-checkbox-bubble-preview',
+  selector: 'checkbox-bubble-preview',
   templateUrl: './checkbox-bubble-preview.html',
-  styleUrls: ['../bubble-preview.scss']
+  styleUrls: ['../bubble-preview.scss', './checkbox-bubble-preview.scss']
 })
 export class CheckboxBubblePreviewComponent implements OnInit {
 
   @Input()
-  checkboxBubble: CheckboxBubble;
-  answersCount: number;
+  bubble: CheckboxBubble;
 
-  ngOnInit(): void {
-   this.answersCount = this.checkboxBubble.options.filter(option => option.answers.length > 0).length;
+  amountOptions;
+  amountUnchecked;
+
+  constructor(public checkboxDataService: CheckboxDataService) {
   }
 
+
+  ngOnInit(): void {
+    this.amountOptions = this.checkboxDataService.getAmountOptions(this.bubble);
+    this.amountUnchecked = this.checkboxDataService.getAmountUnchecked(this.bubble);
+  }
 }
