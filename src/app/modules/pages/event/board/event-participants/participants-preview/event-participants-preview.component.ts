491453f2eef6220ec3ab5a558fc9ab0912189f19
Théo Joubert <theojoubertmoureaud@gmail.com>
2019-04-23 11:51:30 +0000

    Update particpations

--- src/app/modules/pages/event/board/event-participants/participants-preview/event-participants-preview.component.ts
+++ src/app/modules/pages/event/board/event-participants/participants-preview/event-participants-preview.component.ts
@@ -1,10 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import {
-  EventParticipant,
-  ParticipantsSplitted
-} from '../../../../../../shared/models/event-participant';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { EventParticipant, ParticipantsSplitted } from '../../../../../../shared/models/event-participant';
 import { HttpService } from '../../../../../../core/http/http.service';
 import { EventParticipantsService } from '../../../../../../core/services/event-participants.service';
+import { Subscription } from 'rxjs';
 
 declare const $: any;
 
@@ -14,22 +12,40 @@ declare const $: any;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class EventParticipantsPreviewComponent implements OnInit {
+export class EventParticipantsPreviewComponent implements OnInit, OnDestroy {
 
   @Input()
   participationRefs: EventParticipant[];
   participantsSplitted: ParticipantsSplitted;
+  subscriptions: Subscription[] = [];
 
   constructor(private httpService: HttpService,
-              private eventParticipantsService: EventParticipantsService) {
+              private eventParticipantsService: EventParticipantsService,
+              private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
+    this.subscriptions.push(this.eventParticipantsService.participationUpdated$.subscribe((eventParticipation) => {
+      this.participationRefs[this.participationRefs
+        .findIndex(p => p.userRef === eventParticipation.userRef)] = eventParticipation;
+      this.initValues();
+      this.cd.detectChanges();
+    }));
+    this.initValues();
+  }
+
+  initValues() {
     this.participantsSplitted = this.eventParticipantsService.splitParticipantsByResponse(this.participationRefs);
   }
 
   close(): void {
     $('#modalEventParticipants').modal('hide');
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 
 }
