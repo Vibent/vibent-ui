af6406d889f299ee344ab4daa563e57c7e6866c5
unknown <theojoubertmoureaud@gmail.com>
2019-08-13 22:47:35 +0200

    Fix prod build notif

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,14 @@ export const environment = {
   prod: true,
   apiEndpoint: 'https://api.vibent.com',
   blackNoteUrl: 'https://blacknote.vibent.com',
-  appUrl: 'https://vibent.com'
+  appUrl: 'https://vibent.com',
+  firebase: {
+    apiKey: 'AIzaSyBIVfIoFzmUFzJdkDfAlxLZt8UTcIhvTgA',
+    authDomain: 'vibent-1543698003285.firebaseapp.com',
+    databaseURL: 'https://vibent-1543698003285.firebaseio.com',
+    projectId: 'vibent-1543698003285',
+    storageBucket: '',
+    messagingSenderId: '821632933125',
+    appId: '1:821632933125:web:075e677b6444db2d'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@ export const environment = {
   blackNoteUrl: 'https://dev.blacknote.vibent.com',
   appUrl: 'https://dev.vibent.com',
   firebase: {
-    apiKey: "AIzaSyBIVfIoFzmUFzJdkDfAlxLZt8UTcIhvTgA",
-    authDomain: "vibent-1543698003285.firebaseapp.com",
-    databaseURL: "https://vibent-1543698003285.firebaseio.com",
-    projectId: "vibent-1543698003285",
-    storageBucket: "",
-    messagingSenderId: "821632933125",
-    appId: "1:821632933125:web:075e677b6444db2d"
+    apiKey: 'AIzaSyBIVfIoFzmUFzJdkDfAlxLZt8UTcIhvTgA',
+    authDomain: 'vibent-1543698003285.firebaseapp.com',
+    databaseURL: 'https://vibent-1543698003285.firebaseio.com',
+    projectId: 'vibent-1543698003285',
+    storageBucket: '',
+    messagingSenderId: '821632933125',
+    appId: '1:821632933125:web:075e677b6444db2d'
   }
 };
